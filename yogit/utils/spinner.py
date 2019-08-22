eb22223b13ad3f18d267e757b6f7f9d6580b5afc
Adrien Gavignet <adrien.gavignet@gmail.com>
2019-08-23 01:55:16 +0200

    Extra spinner creation object in a function

--- yogit/utils/spinner.py
+++ yogit/utils/spinner.py
@@ -3,17 +3,21 @@ import platform
 from halo import Halo
 
 
+def get_spinner_object():
+    spinner_type = "dots"
+    if platform.system() == "Windows":
+        # Avoid unicode char issue
+        spinner_type = "line"
+    return Halo(text="Loading", spinner=spinner_type, color=None)
+
+
 def spin(func):
     """
    Wrap Halo within a decorator then it's easy to disable it.
    """
 
     def inner(self, *args, **kwargs):
-        spinner_type = "dots"
-        if platform.system() == "Windows":
-            # Avoid unicode char issue
-            spinner_type = "line"
-        with Halo(text="Loading", spinner=spinner_type, color=None) as spinner:
+        with get_spinner_object() as spinner:
             func(self, spinner, *args, **kwargs)
 
     return inner
