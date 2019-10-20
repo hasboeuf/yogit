9181a0b93fe713eda7352ca3501483b51ff59b5c
Adrien Gavignet <adrien.gavignet@gmail.com>
2019-10-20 14:26:02 +0200

    Change slack integration in scrum report
    - introduce SlackRESTClient, SlackQuery and SlackPostMessageQuery classes
    - dont propose to post on Slack if not configured
    - update tests
    - remove thread post feature as it's not generic enough,
      will be plugged back in future commit

--- yogit/yogit/slack.py
+++ yogit/yogit/slack.py
@@ -4,34 +4,69 @@ Send report to slack
 import json
 from yogit.yogit.settings import Settings
 from yogit.api.requester import http_call
+from yogit.utils.spinner import spin
+from yogit.yogit.logger import LOGGER
+
+SLACK_API_URL = "https://slack.com/api"
+SLACK_POST_MESSAGE_ENDPOINT = "/chat.postMessage"
 
 
-def _send_to_slack(text, thread_id=None):
-    settings = Settings()
-    token = settings.get_slack_token()
-    channel = settings.get_slack_channel()
+class SlackRESTClient:
+    """
+    Slack REST API client
+    """
 
-    webhook_url = "https://slack.com/api/chat.postMessage"
+    def _get_url(self, endpoint):
+        return SLACK_API_URL + endpoint
 
-    if thread_id:
-        slack_data = {
-            "token": token,
-            "channel": channel,
-            "text": "```{}```".format(text),
-            "as_user": "True",
-            "thread_ts": thread_id,
+    def post(self, endpoint, payload):
+        """
+        Perform POST Slack REST request
+        """
+        url = self._get_url(endpoint)
+        LOGGER.debug("POST %s", url)
+        LOGGER.debug(payload)
+        return http_call("post", url, data=payload)
+
+
+class SlackQuery:
+    """ Represent a Slack query """
+
+    def __init__(self, endpoint):
+        self.response = None
+        self.client = SlackRESTClient()
+        self.endpoint = endpoint
+        self.payload = {}
+
+    def _handle_response(self, response):
+        raise NotImplementedError()
+
+    @spin
+    def execute(self, spinner):
+        """ Execute the query """
+        response = self.client.post(self.endpoint, self.payload)
+        self._handle_response(response)
+
+
+class SlackPostMessageQuery(SlackQuery):
+    def __init__(self, message, reply_to=None):
+        super().__init__(SLACK_POST_MESSAGE_ENDPOINT)
+        self.message = message
+        self.reply_to = reply_to
+        self.thread_id = None
+        self.payload = self._get_payload()
+
+    def _get_payload(self):
+        settings = Settings()
+        payload = {
+            "token": settings.get_slack_token(),
+            "channel": settings.get_slack_channel(),
+            "text": self.message,
+            "as_user": True,
         }
-    else:
-        slack_data = {"token": token, "channel": channel, "text": text, "as_user": "True"}
+        if self.reply_to is not None:
+            payload.update({"thread_ts": self.reply_to.thread_id})
+        return payload
 
-    return http_call("post", webhook_url, data=slack_data)
-
-
-def send_report_to_slack(report):
-
-    report = report.split("```")
-    report_text = report[0]
-    report_git = report[1]
-
-    message = _send_to_slack(report_text)
-    _send_to_slack(report_git, thread_id=message.json()["ts"])
+    def _handle_response(self, response):
+        self.thread_id = response.get("ts")
