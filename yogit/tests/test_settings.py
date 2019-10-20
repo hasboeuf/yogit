dd5a8ac9826adf7e5252cf9c0c90ce23fa4486ff
Adrien Gavignet <adrien.gavignet@gmail.com>
2019-10-20 21:05:53 +0200

    Test report migration (1 to 2)

--- yogit/tests/test_settings.py
+++ yogit/tests/test_settings.py
@@ -1,8 +1,47 @@
 import pytest
 
 
-from yogit.tests.mocks.mock_settings import temporary_settings, assert_empty_settings
-from yogit.yogit.settings import Settings
+from yogit.tests.mocks.mock_settings import temporary_settings, assert_empty_settings, temporary_scrum_report
+from yogit.yogit.settings import Settings, ScrumReportSettings
+
+REPORT_TEMPLATE_1 = r"""
+questions:
+- What have you done today?
+- Do you have any blockers?
+- What do you plan to work on your next working day?
+template:
+- *REPORT ${today}*
+- *${q0}*
+- ${a0}
+- *${q1}*
+- ${a1}
+- *${q2}*
+- ${a2}
+-
+- ```
+- ${github_report}
+- ```
+version: 1
+"""
+
+REPORT_TEMPLATE_2 = r"""questions:
+- What have you done today?
+- Do you have any blockers?
+- What do you plan to work on your next working day?
+template:
+    sections:
+    -   - '*REPORT ${date}*'
+        - '*${q0}*'
+        - ${a0}
+        - '*${q1}*'
+        - ${a1}
+        - '*${q2}*'
+        - ${a2}
+    -   - '```'
+        - ${github_report}
+        - '```'
+version: 2
+"""
 
 
 @pytest.mark.usefixtures("temporary_settings")
@@ -39,3 +78,17 @@ def test_set_get_reset_settings():
     settings.reset_github()
     assert not settings.is_github_valid()
     assert_empty_settings()
+
+
+@pytest.mark.usefixtures("temporary_scrum_report")
+def test_report_settings_1_to_2_migration():
+    report_settings = ScrumReportSettings()
+
+    with open(report_settings.storage.get_path(), "w") as settings_file:
+        settings_file.write(REPORT_TEMPLATE_1)
+
+    report_settings.get()
+
+    with open(report_settings.storage.get_path(), "r") as settings_file:
+        content = settings_file.read()
+        assert content == REPORT_TEMPLATE_2
